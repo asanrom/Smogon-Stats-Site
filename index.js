abe4e52e89a4b38c85593f633d8a732735fd4c71
Agustin San Roman <agustinsanromanguzman@gmail.com>
2018-05-29 16:57:26 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+/**
+ * Main file
+ * Smogon Stats Site - https://github.com/asanrom/Smogon-Stats-Site
+ * 
+ * Run this file in order to start the application.
+ */
+
+'use strict';
+
+module.exports = require("./dist/app.js");
---
+++ src/app.ts
@@ -0,0 +1,9 @@
+/**
+ * Application
+ * Smogon Stats Site - https://github.com/asanrom/Smogon-Stats-Site
+ * 
+ * Main application object. Loads the modules and starts the server.
+ */
+
+'use strict';
+
